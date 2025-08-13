551e70c674997e061b48ecf3e38f07253f9b0658
igor.boenavides <igor.boenavides@proton.me>
2025-08-13 18:10:22 -0300

    Made it classy i guess

--- password-strength-checker.py
+++ password-strength-checker.py
@@ -1,56 +1,60 @@
 import string
 
+
 def main():
-    print("===========================================")
     print("WELCOME TO THE PASSWORD strength CHECKER!!!")
-    print("===========================================")
-    print()
-    password = input("Inform the password for the strength check: ")
-    strength_check(password)
+    passwd = input("Inform the password for the strength check: ")
+    score = 0
+    digit_minimum(passwd, score)
+    upperlower(passwd, score)
+    has_digit(passwd, score)
+    has_symbols(passwd, score)
 
-def strength_check(passwd):
-    strength_score = 0
-
-    # ----- At least 8 digits (2) -----
+# ----- At least 8 digits (2) -----
+def digit_minimum(passwd, score):
     if len(passwd) >= 8:
-        strength_score += 2
+        score += 2
     else:
         print("Your password is too short.")
 
-    # ----- A combination of lower and uppercase letter (4) -----
+
+# ----- A combination of lower and uppercase letter (4) -----
+def upperlower(passwd, score):
     if any(char.islower() for char in passwd) and any(
         char.isupper() for char in passwd
     ):
-        strength_score += 4
+        score += 4
     else:
-        print(
-            "The password does not contain a combination of uppercase adn lowercase letters."
-        )
+        print("The password does not contain a combination of uppercase adn lowercase letters.")
 
-    # ----- Numbers (10) -----
+
+# ----- Numbers (10) -----
+def has_digit(passwd, score):
     if any(char.isdigit() for char in passwd):
-        strength_score += 10
+        score += 10
     else:
         print("The password does not contain any numbers.")
 
-    # ----- Symbols (! @ # $) (10) -----
+# ----- Symbols (! @ # $) (10) -----
+def has_symbols(passwd, score):
     symbols = list(string.punctuation)
     if any(char in symbols for char in passwd):
-        strength_score += 10
+        score += 10
     else:
         print(
             "The password does not contain symbols."
         )
 
-    # ----- Inform score (max. = 26) -----
-    if strength_score >= 17.33:
-        print(f"Password points: {strength_score}")
+# ----- Inform score (max. = 26) -----
+def tell_score(score):
+    if score >= 17.33:
+        print(f"Password points: {score}")
         print("Your password is STRONG!!! 💪 💪 💪")
-    elif strength_score >= 8.67:
-        print(f"Password points: {strength_score}")
+    elif score >= 8.67:
+        print(f"Password points: {score}")
         print("Your password is average. 😑 😑 😑")
     else:
-        print(f"Password points: {strength_score}")
+        print(f"Password points: {score}")
         print("Your password is WEAK. 💩 💩 💩")
 
 
